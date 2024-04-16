1ce5fbefe2fdff01f4c02ff50ff50283bc680ae2
Akhtar Abbas <akhtarabbas@Akhtars-MacBook-Air.local>
2024-04-17 02:59:45 +0530

    cleaning

--- Models/investor/displayPortfolio.h
+++ Models/investor/displayPortfolio.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 void Investor::displayPortfolio(){
-        cout << "Portfolio for user " << getName() << ":" << endl;
+        cout << "Portfolio for user: " << getName() << ":" << endl;
         for (const auto &entry : portfolio)
         {
             const string &company = entry.first;
